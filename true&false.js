233900c9c7fbdd4dc8e6ef74630f220980921005
Fcode9 <50692937+Fcode9@users.noreply.github.com>
2021-03-02 19:50:46 +0600

    truthy and falsy value complite

---
+++ true&false.js
@@ -0,0 +1,22 @@
+const name = "nothing";
+if (name){
+    console.log('this is true');
+} else {
+    console.log ('this is false ');
+};
+
+ const name2 ="";
+ if (name2){
+     console.log('this is true');
+ } else{
+     console.log( " this is false");
+ };
+
+ /*falsy value : 
+ 1. 0;
+ 2. null;
+ 3.undefinde;
+ 4. "";
+ 5. nan;
+ 6. false
+ */
