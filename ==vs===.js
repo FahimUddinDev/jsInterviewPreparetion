84229186f420fa53a8f253da6bc32048cf4430c2
Fcode9 <50692937+Fcode9@users.noreply.github.com>
2021-03-02 23:32:08 +0600

    == and === comparisiom

---
+++ ==vs===.js
@@ -0,0 +1,36 @@
+const first = 2;
+const second = 2;
+if (first == second){
+    console.log("condition is true");
+} else{
+    console.log("Condition is False");
+};
+ const num = 1;
+ const condition = true;
+ if (num == condition){
+     console.log("this is 1 is true");
+ } else {
+     console.log('this 1 is not true');
+ };
+
+// == only check value is same or not
+const third = 2;
+const fourth = "2";
+if (third == fourth){
+    console.log("condition is true");
+} else{
+    console.log("Condition is False");
+};
+// === check value and value type is same or not 
+
+if (third === fourth){
+    console.log("condition is true");
+} else{
+    console.log("Condition is False");
+};
+
+if (num === condition){
+    console.log("this is 1 is true");
+} else {
+    console.log('this 1 is not true');
+}; 
