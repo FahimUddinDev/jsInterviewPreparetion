a0b779e7820682c6392564a0d1aae9f80eef61f5
Fcode9 <50692937+Fcode9@users.noreply.github.com>
2021-03-07 13:18:36 +0600

    new keywoard

---
+++ newKeyword.js
@@ -0,0 +1,12 @@
+// make a class 
+class pepole{
+    constructor(firstName, lastName, phone, selery){
+        this.firstName= firstName;
+        this.lastName = lastName;
+        this.phone=phone;
+        this.selery = selery;
+    };
+};
+
+const fahim = new pepole('Fahim','Uddin',01704880254, 13000);
+console.log(fahim);
